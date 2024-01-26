58344538e66a585b96d4010f838694a4514335c5
mini-xi <gam1205@naver.com>
2024-01-26 09:49:36 +0900

    Trainer Bag, Trainer Service 추가 및 기능 정의

--- src/main/java/com/ohgiraffers/toyproject/service/TrainerService.java
+++ src/main/java/com/ohgiraffers/toyproject/service/TrainerService.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.toyproject.service;
 
+import com.ohgiraffers.toyproject.aggregate.TrainerBag;
+
 import java.util.Scanner;
 
 public class TrainerService {
@@ -16,31 +18,42 @@ public class TrainerService {
                 /* 포켓몬 공격2 */
                 break;
         }
-        System.out.println("가 공격을 명령합니다.");
+        System.out.println("가 공격을 명령합니다.");    // 포켓몬 기술과 기술 메소드에서 받아서 사용
     }
 
     // 도망가기
     public void runAway() {
         System.out.println("배틀을 종료합니다.");
-        /* 배틀 나가는거 불러오기 */
+        return;
     }
 
     //가방 열기
     public void openBag() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1. 회복약 사용 2. 몬스터볼 사용 3. 갯수 확인");
+        int input = sc.nextInt();
+        switch (input) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                /* ("현재 사용가능한 갯수는 회복약 \'" + + "\'개, 몬스터볼 \'" + + "\'개 입니다." ) */
+                break;
+        }
     }
 
     // 회복약 사용하기
     public void useHealItem() {
-        System.out.println("회복약을 사용합니다.");
-    }
-
-    private void setHealingPack() {
-
+        TrainerBag.useHealItem();
+        System.out.println("포켓몬을 회복시킵니다");
+        System.out.println("남은 회복약은" + TrainerBag.getHealItemConut() + "개 입니다.");
     }
 
     // 포켓몬볼 던지기
     public void throwingBall() {
-        System.out.println("몬스터볼을 사용합니다. ");
+        TrainerBag.useMonsterBall();
+        System.out.println("몬스터볼을 사용합니다.");
+        System.out.println("남은 몬스터볼은 " + TrainerBag.getMonsterBallCount() + "개 입니다.");
     }
 }
