1588e529eaca8b99f917a9a78bb092034b19282c
DESKTOP-D4SBG1D\Playdata <goeunpark21@gmail.com>
2024-01-31 16:01:00 +0900

    Revert "[UPDATE] exception 폴더 주석 추가"
    This reverts commit d9ec56c1a894ba135e69ad80bbb7755f056a958b.

--- src/main/java/com/ohgiraffers/toyproject/exception/ChoiceException.java
+++ src/main/java/com/ohgiraffers/toyproject/exception/ChoiceException.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.toyproject.exception;
 
-/*설명. 사용자가 선택지에 없는 번호를 입력했을 경우에 발생하는 예외 처리*/
 public class ChoiceException extends Exception{
     public ChoiceException(String message) {
         super(message);
