c2f039ec25e9e8fd8b1ad4b0d907e5f80bb0e976
mini-xi <gam1205@naver.com>
2024-01-31 16:28:41 +0900

    주석 말 변경

--- src/main/java/com/ohgiraffers/toyproject/exception/ChoiceException.java
+++ src/main/java/com/ohgiraffers/toyproject/exception/ChoiceException.java
@@ -1,6 +1,6 @@
 package com.ohgiraffers.toyproject.exception;
 
-/*설명. 사용자가 선택지에 없는 번호를 입력했을 경우에 발생하는 예외 처리*/
+/* 설명. 사용자가 선택지에 없는 번호를 입력했을 경우에 발생하는 예외를 처리한다. */
 public class ChoiceException extends Exception{
     public ChoiceException(String message) {
         super(message);
--- src/main/java/com/ohgiraffers/toyproject/exception/NotEnoughMonsterBall.java
+++ src/main/java/com/ohgiraffers/toyproject/exception/NotEnoughMonsterBall.java
@@ -1,6 +1,6 @@
 package com.ohgiraffers.toyproject.exception;
 
-/*설명. 사용자가 몬스터볼을 모두 소진하고서 몬스터볼을 다시 사용하려는 경우에 발생하는 예외 처리*/
+/* 설명. 사용자가 몬스터볼을 모두 소진하고서 몬스터볼을 다시 사용하려는 경우에 발생하는 예외를 처리한다. */
 public class NotEnoughMonsterBall extends Exception{
     public NotEnoughMonsterBall(String message) {
         super(message);
