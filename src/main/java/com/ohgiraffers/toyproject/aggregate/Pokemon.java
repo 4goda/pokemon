def3bdc3a4e5080f77504fe53078f48433cee588
hso <sh07202@naver.com>
2024-01-31 16:10:45 +0900

    [UPDATE] Pokemon 주석 추가

--- src/main/java/com/ohgiraffers/toyproject/aggregate/Pokemon.java
+++ src/main/java/com/ohgiraffers/toyproject/aggregate/Pokemon.java
@@ -43,45 +43,41 @@ public abstract class Pokemon implements Serializable {
         return sound;
     }
 
-    public boolean getIsAlive() {
-        return isAlive;
-    }
-
     public int getHp() {
         return hp;
     }
-    
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    /* 설명. 전달된 skillNum값에 따라 현재 포켓몬이 보유한 기본 스킬 또는 special 스킬명을 반환한다. */
     public String attack(int skillNum){
         if(skillNum == 1)
             return defaultAttack();
         return specialAttack();
     }
 
+    /* 설명. 포켓몬의 기본 스킬명을 반환한다. */
     public String defaultAttack(){
         return Pokemon.DEFAULT_SKILL;
     }
 
+    /* 설명. 각 포켓몬마다 다르게 보유한 스킬명을 반환한다.(포켓몬마다 보유한 스킬이 다르므로 추상메소드로 선언) */
     public abstract String specialAttack();
 
+    /* 설명. 공격받은 데미지를 전체 hp에서 감소한 후, 현재 포켓몬의 hp가 0 이하일 경우 isAlive 상태를 바꿔준다. */
     public void receiveDamage(int damage){
         hp -= damage;
         if(hp <= 0) isAlive = false;
     }
 
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
+    /* 설명. 포켓몬에게 회복약을 사용했을 경우, 전달된 회복량만큼 포켓몬의 hp를 증가시킨다.(회복할 수 있는 최대값은 현재 포켓몬의 maxHp로 한다.) */
     public void healPokemon(int recovery){
         hp += recovery;
         if(hp >= maxHp) {
             hp = maxHp;
         }
-    }
-
-    public void run(){
-        isAlive = false;
     }
 
     @Override
